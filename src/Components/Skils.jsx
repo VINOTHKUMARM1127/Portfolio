06b0c6398047f00308efb7bae9e217587a44475e
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-01-23 23:14:14 +0530

    Minor changes

--- src/Components/Skils.jsx
+++ src/Components/Skils.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Icon } from "@iconify/react";
 
 const Skils = () => {
   return (
-    <div>
-      <div className=''>Skills</div>
+    <div className="mt-5">
+      <div className="text-center text-[1.8em] font-bold">Skills</div>
+      <div className="flex gap-1 md:gap-10 flex-col md:flex-row">
+        <div className="">
+          <Icon icon="vscode-icons:file-type-html" width="40" height="40" />
+          HTML
+        </div>
+        <div className="">
+          <Icon
+            icon="flowbite:css-solid"
+            width="40"
+            height="40"
+            className="text-[#1600ff]"
+          />
+          CSS
+        </div>
+        <div className="">
+          <Icon
+            icon="vscode-icons:file-type-js-official"
+            width="40"
+            height="40"
+          />
+          JAVASCRIPT
+        </div>
+        <div className="">
+          <Icon icon="logos:react" width="40" height="40" />
+          REACT
+        </div>
+        <div className="">
+          <Icon icon="skill-icons:tailwindcss-dark" width="40" height="40" />
+          TAILWIND CSS
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skils
+export default Skils;
