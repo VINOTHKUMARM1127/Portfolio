c3173cc3116eef2c32deb64a59c273b651dd5808
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-02-02 21:05:39 +0530

    adding skills

--- src/Components/Skils.jsx
+++ src/Components/Skils.jsx
@@ -3,42 +3,59 @@ import { Icon } from "@iconify/react";
 
 const Skils = () => {
   return (
-    <div className="mt-5">
+    <div className="mt-5 w-screen md:w-[80%] mx-auto my-0">
       <div className="text-center text-[2em] font-bold mt-3">Skills</div>
       <div className="text-center text-[1.2em] opacity-55 mt-5 mb-10">
         Here are some of my skills on which I have learnt.
       </div>
-      <div className="flex justify-center md:justify-evenly text-center flex-col md:flex-row">
-        <div className="border border-blue-600 rounded-xl px-0 mx-[10%] my-5 md:px-16 pb-8 md:mx-0 md:my-0 opacity-80 shadow-[0_0_10px_blue]">
-          <div className="text-center text-[1.6em] my-4 font-bold">Frontend</div>
-          <div className="flex justify-center gap-5">
-            <h3 className="border border-white rounded-lg px-3 py-2">HTML</h3>
-            <h3 className="border border-white rounded-lg px-3 py-2">CSS</h3>
-            <h3 className="border border-white rounded-lg px-3 py-2">
+      <div className="flex justify-center md:justify-evenly text-center flex-col lg:flex-row">
+        <div className="border border-[#b14fc4] rounded-xl px-0 mx-[10%] my-5 md:px-16 pb-8 lg:mx-0 lg:my-0 opacity-80 shadow-[0_0_10px_#d607ed]">
+          <div className="text-center text-[1.8em] my-4 font-bold">Frontend</div>
+          <div className="flex justify-center gap-5 flex-wrap px-1">
+            <h3 className="border border-white rounded-lg px-3 py-2 flex justify-center items-center gap-2">
+            <Icon icon="vscode-icons:file-type-html" width="32" height="32" />
+              HTML</h3>
+            <h3 className="border border-white rounded-lg px-3 py-2 flex justify-center items-center gap-2">
+            <Icon icon="vscode-icons:file-type-css2" width="32" height="32" />
+              CSS</h3>
+            <h3 className="border border-white rounded-lg px-3 py-2 flex justify-center items-center gap-2">
+            <Icon icon="logos:javascript" width="32" height="32" />
               JavaScript
             </h3>
           </div>
-          <div className="flex justify-center gap-5 mt-3">
+          <div className="flex justify-center gap-5 mt-3 flex-wrap px-1">
             
-            <h3 className="border border-white rounded-lg px-3 py-2">
+            <h3 className="border border-white rounded-lg px-3 py-2 flex justify-center items-center gap-2">
+            <Icon icon="skill-icons:react-dark" width="32" height="32" />
               React Js
             </h3>
-            <h3 className="border border-white rounded-lg px-3 py-2">
+            <h3 className="border border-white rounded-lg px-3 py-2 flex justify-center items-center gap-2">
+            <Icon icon="skill-icons:tailwindcss-dark" width="32" height="32" />
               Tailwind CSS
             </h3>
           </div>
         </div>
 
-        <div className="border border-blue-600 rounded-xl px-0 mx-[10%] my-5 md:px-16 pb-8 md:mx-0 md:my-0 opacity-80 shadow-[0_0_10px_blue]">
-        <div className="text-center text-[1.6em] my-4 font-bold">Others</div>
-          <div className="flex justify-center gap-5">
-            <h3 className="border border-white rounded-lg px-3 py-2">Git</h3>
-            <h3 className="border border-white rounded-lg px-3 py-2">GitHub</h3>
-            <h3 className="border border-white rounded-lg px-3 py-2">PhotoShop</h3>
+        <div className="border border-[#b14fc4] rounded-xl px-0 mx-[10%] my-5 md:px-10 pb-8 lg:mx-0 lg:my-0 opacity-80 shadow-[0_0_10px_#d607ed]">
+        <div className="text-center text-[1.8em] my-4 font-bold">Others</div>
+          <div className="flex justify-center gap-5 flex-wrap px-1">
+            <h3 className="border border-white rounded-lg px-3 py-2 flex justify-center items-center gap-2">
+            <Icon icon="devicon:git" width="32" height="32" />
+              Git</h3>
+            <h3 className="border border-white rounded-lg px-3 py-2 flex justify-center items-center gap-2">
+            <Icon icon="fa6-brands:square-github" width="32" height="32" />
+              GitHub</h3>
+            <h3 className="border border-white rounded-lg px-3 py-2 flex justify-center items-center gap-2">
+            <Icon icon="skill-icons:photoshop" width="32" height="32" />
+              PhotoShop</h3>
           </div>
-          <div className="flex justify-center gap-5 mt-3">
-            <h3 className="border border-white rounded-lg px-3 py-2">Netlify</h3>
-            <h3 className="border border-white rounded-lg px-3 py-2">Vite</h3>
+          <div className="flex justify-center gap-5 mt-3 flex-wrap px-1">
+            <h3 className="border border-white rounded-lg px-3 py-2 flex justify-center items-center gap-2">
+            <Icon icon="vscode-icons:file-type-netlify" width="32" height="32" />
+              Netlify</h3>
+            <h3 className="border border-white rounded-lg px-3 py-2 flex justify-center items-center gap-2">
+            <Icon icon="skill-icons:vite-light" width="32" height="32" />
+              Vite</h3>
           </div>
         </div>
       </div>
