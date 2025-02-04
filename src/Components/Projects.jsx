171cb4bd9dc72953e448a62c99a113d2870b8b51
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-02-04 22:22:04 +0530

    add Education

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import LotGames from '../assets/LotGames.png'
+import LotHub from '../assets/LotHub.png'
 
 const Projects = () => {
   return (
@@ -9,31 +11,34 @@ const Projects = () => {
         Here are some of my projects.
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3">
-        <div className="border border-[#b14fc4] rounded-xl w-[70%] md:w-[70%] py-4 px-2 mx-auto my-0 opacity-80 shadow-[0_0_6px_#d607ed] mb-8">
-          <div className=" px-5 text-[1.2em] font-bold opacity-90 ">LotHub</div>
-          <div className=" px-5 text-[0.8em] opacity-50 mb-1">2024</div>
-          <div className=" px-5 text-[0.9em] opacity-70 text-justify">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-[90%] md:w-[100%] mx-auto my-0">
+        <div className="border border-[#b14fc4] rounded-xl py-4 px-2 mx-auto my-0 opacity-80 shadow-[0_0_6px_#d607ed] mb-8">
+        <img src={LotHub} alt="" className="rounded-[10%] min-h-[180px] min-w-[280px] px-4 mb-2"/>
+          <div className=" px-4 text-[1.2em] font-bold opacity-90 ">LotHub</div>
+          <div className=" px-4 text-[0.8em] opacity-50 mb-1">2024</div>
+          <div className=" px-4 text-[0.9em] opacity-70 text-justify">
             As a Computer Science graduate, I possess strong problem-solving and
             precision skills. Combining this foundation with my passion for
             technology, I'm venturing into IT.
           </div>
         </div>
 
-        <div className="border border-[#b14fc4] rounded-xl w-[70%] md:w-[70%] py-4 px-2 mx-auto my-0 opacity-80 shadow-[0_0_6px_#d607ed] mb-8">
-          <div className=" px-5 text-[1.2em] font-bold opacity-90 ">LotHub</div>
-          <div className=" px-5 text-[0.8em] opacity-50 mb-1">2024</div>
-          <div className=" px-5 text-[0.9em] opacity-70 text-justify">
+        <div className="border border-[#b14fc4] rounded-xl py-4 px-2 mx-auto my-0 opacity-80 shadow-[0_0_6px_#d607ed] mb-8">
+        <img src={LotGames} alt="" className="rounded-[10%] min-h-[180px] min-w-[280px] px-4 mb-2"/>
+          <div className=" px-4 text-[1.2em] font-bold opacity-90 ">LotGames</div>
+          <div className=" px-4 text-[0.8em] opacity-50 mb-1">2024</div>
+          <div className=" px-4 text-[0.9em] opacity-70 text-justify">
             As a Computer Science graduate, I possess strong problem-solving and
             precision skills. Combining this foundation with my passion for
             technology, I'm venturing into IT.
           </div>
         </div>
 
-        <div className="border border-[#b14fc4] rounded-xl w-[70%] md:w-[70%] py-4 px-2 mx-auto my-0 opacity-80 shadow-[0_0_6px_#d607ed] mb-8">
-          <div className=" px-5 text-[1.2em] font-bold opacity-90 ">LotHub</div>
-          <div className=" px-5 text-[0.8em] opacity-50 mb-1">2024</div>
-          <div className=" px-5 text-[0.9em] opacity-70 text-justify">
+        <div className="border border-[#b14fc4] rounded-xl py-4 px-2 mx-auto my-0 opacity-80 shadow-[0_0_6px_#d607ed] mb-8">
+          <img src={LotGames} alt="" className="rounded-[10%] min-h-[180px] min-w-[280px] px-4 mb-2"/>
+          <div className=" px-4 text-[1.2em] font-bold opacity-90 ">LotHub</div>
+          <div className=" px-4 text-[0.8em] opacity-50 mb-1">2024</div>
+          <div className=" px-4 text-[0.9em] opacity-70 text-justify">
             As a Computer Science graduate, I possess strong problem-solving and
             precision skills. Combining this foundation with my passion for
             technology, I'm venturing into IT.
