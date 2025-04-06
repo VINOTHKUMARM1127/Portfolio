4fc4826911aedf5906758fd9831507a4dc34f41f
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-06 23:21:13 +0530

    Adding bg

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
         <Link to="education" smooth={true} duration={500} offset={-80} className='md:mx-[2em] mx-[1em] hover:text-violet-600'>Education</Link>
         <Link to="projects" smooth={true} duration={500} offset={-80} className='md:mx-[2em] mx-[1em] hover:text-violet-600'>Projects</Link>
       </div></div>
-      <a href='https://github.com/VINOTHKUMARM1127' className='mx-0 my-auto border-[2px] border-[#6536ff] px-6 py-1  rounded-full text-[#6536ff]'>Github</a>
+      <a href='https://github.com/VINOTHKUMARM1127' className='mx-0 my-auto border-[2px] border-[#6536ff] px-6 py-1  rounded-full text-[#6536ff] hover:bg-violet-600 hover:text-white'>Github</a>
     </div>
   )
 }
