f77df12eca1e60ca97fb52237d6e0a380331deae
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-12 20:01:52 +0530

    minor changes

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
     return (
         <section className='bg-[#171721] cut flex justify-center py-[5em]'>
         <div className='w-[100%] lg:w-[90%] mx-auto my-0 flex flex-col-reverse lg:flex-row justify-evenly items-center'>
-            <div className='text-start w-[90vw] lg:w-[40vw]'>
+            <div className='text-center lg:text-start w-[90vw] lg:w-[40vw]'>
                 <div className='text-[1.7em] md:text-[3em] font-black mt-4 md:mt-0'>
                 <div className='text-[1.4em] md:text-[1em] '>Hi I Am</div>
                 <div className='text-[1.4em] md:text-[1em]'>Vinoth Kumar M</div>
@@ -17,7 +17,7 @@ const Main = () => {
                      staying updated on frontend tech. Collaborative and committed to quality,
                       you thrive in dynamic teams. Eager to contribute creativity and bring 
                       innovative ideas to life.</div>
-                <div className='text-[1.1em] bg-gradient-to-r from-purple-700 to-blue-700 w-fit px-8 py-4 rounded-lg my-6 cursor-pointer hover:scale-105'>Check Resume</div>
+                <div className='text-[1.1em] bg-gradient-to-r from-purple-700 to-blue-700 w-fit px-8 py-4 rounded-lg mx-auto lg:mx-0 my-6 cursor-pointer hover:scale-105 '>Check Resume</div>
             </div>
 
             <div>
